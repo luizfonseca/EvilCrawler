aef9729a6cf2488e7bcbe6e0edb6eac6782153ba
Luiz Fonseca <runeroniek@gmail.com>
2011-04-24 14:04:54 -0400

    ImageCrawler methods created

--- classes/ImageCrawler.rb
+++ classes/ImageCrawler.rb
@@ -1,3 +1,18 @@
 class ImageCrawler
+  attr_accessor :query, :resolution
+   
+  def initialize(query, resolution)
+    @query = query
+    @resolution = resolution
+  end
+
+  def do_search
+    
+  end
+  
+  
+  def filter_resolution
+    
+  end
 
 end
