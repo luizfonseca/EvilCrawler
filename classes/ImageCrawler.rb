9af1baa9d81e441034196842c6c00ffcb2bb1354
Luiz Fonseca <runeroniek@gmail.com>
2011-04-24 17:58:17 -0400

    Everything working, take_image method created

--- classes/ImageCrawler.rb
+++ classes/ImageCrawler.rb
@@ -1,9 +1,11 @@
 require 'nokogiri'
 require 'open-uri'
-require 'net/http'
+require 'rubygems'
+require 'mechanize'
 
 class ImageCrawler
   attr_accessor :query, :resolution
+  $image_folder = File.join(ENV['HOME'],"Projects/image_crawler/data/")
   $google_url = "http://www.google.com.br/images?hl=pt-BR&source=hp&q="
   
   def initialize(query, resolution)
@@ -18,19 +20,17 @@ class ImageCrawler
       src = i.attribute('src')
       file_url = src.to_s.split('http://')[2]
       file_name = file_url.to_s.split('/')[-1]
-      self.take_image(file_url, file_name)
+      if file_name != nil && file_url != nil
+        self.take_image(file_url, file_name)
+      end
     end
   end
   
-  
   def take_image (link, name)
-    u = Net::HTTP.start(link.to_s.split('/')[0][1])
-    response, data = u.get(link)
-    if resp.message == "OK"
-      File.open(name, 'wb+') do |f|
-        f << data
-      end
-    end     
+    link = "http://#{link}"
+    puts "Saved file #{name} in Data folder."
+    agent = Mechanize.new
+    agent.get(link).save_as($image_folder + "#{name}")
   end
   
   def filter_resolution
