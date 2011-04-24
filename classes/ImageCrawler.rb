50e03f235ea4a4e5ba4b3005f27b6f56838d5d8d
Luiz Fonseca <runeroniek@gmail.com>
2011-04-24 14:42:42 -0400

    Some updates

--- classes/ImageCrawler.rb
+++ classes/ImageCrawler.rb
@@ -1,13 +1,20 @@
+require 'open-uri'
+
 class ImageCrawler
   attr_accessor :query, :resolution
-   
+  @google_url = "http://www.google.com.br/images?q=runeroniek"
+  
   def initialize(query, resolution)
     @query = query
     @resolution = resolution
   end
 
   def do_search
-    
+    open('http://www.google.com.br/images?q=runeroniek') do |f|
+      f.each do |line|
+        print "#{line}"
+      end
+    end
   end
   
   
---
+++ init.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+require './classes/ImageCrawler.rb'
+
+image = ImageCrawler.new("a","a")
+image.do_search
